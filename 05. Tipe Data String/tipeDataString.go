d00be622a906642384cb2a730241ec88828a5d3d
Prasetiyo <84963363+prasetiyo-2020@users.noreply.github.com>
2022-01-17 19:03:26 +0700

    update

---
+++ 05. Tipe Data String/tipeDataString.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Prasetiyo")
+	fmt.Println("Mata pelajaran yang saya sukai adalah Matematika")
+
+	fmt.Println(len("Prasetiyo"))
+	fmt.Println(len("Mata pelajaran yang saya sukai adalah Matematika"))
+
+	fmt.Println("Prasetiyo"[0])
+	fmt.Println("Mata pelajaran yang saya sukai adalah Matematika"[0])
+}
